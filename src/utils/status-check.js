606f96b2498c0b15c89255bf6fa5a1bf3c9c2faa
Oli Evans <oli@tableflip.io>
2022-05-04 19:24:29 -0400

    fix: really dont modify immutable headers
    based on https://developers.cloudflare.com/workers/examples/cache-api/
    
    License: (Apache-2.0 AND MIT)
    Signed-off-by: Oli Evans <oli@tableflip.io>

--- src/utils/status-check.js
+++ src/utils/status-check.js
@@ -1,13 +1,13 @@
 export async function pickGatewayHost (cache, env) {
-  let statusResponse = await cache.match(env.STATUS_CHECK_URL)
-  if (!statusResponse) {
+  let response = await cache.match(env.STATUS_CHECK_URL)
+  if (!response) {
     // Get from origin and cache it if not cached
-    statusResponse = await fetch(env.STATUS_CHECK_URL)
-    const clone = statusResponse.clone()
-    clone.headers.set('Cache-Control', `public, max-age=60`)
-    cache.put(env.STATUS_CHECK_URL, clone)
+    response = await fetch(env.STATUS_CHECK_URL)
+    response = new Response(response.body, response); 
+    response.headers.set('Cache-Control', `public, max-age=60`)
+    cache.put(env.STATUS_CHECK_URL, response.clone())
   }
-  const checker = await statusResponse.json()
+  const checker = await response.json()
 
   // Get URL to redirect
   return checker.status === 'ok' ? env.IPFS_GATEWAY_HOSTNAME : env.ipfsGateways[0]
