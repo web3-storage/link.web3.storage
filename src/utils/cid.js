561f1890446e1a3c404f1130f1b7f5027bfede9f
Oli Evans <oli@tableflip.io>
2022-05-04 16:57:38 -0400

    feat: any-url style redirection
    see: https://github.com/nftstorage/nft.storage/issues/1435
    
    License: (Apache-2.0 AND MIT)
    Signed-off-by: Oli Evans <oli@tableflip.io>

--- src/utils/cid.js
+++ src/utils/cid.js
@@ -32,3 +32,41 @@ export function normalizeCid(cid) {
   const c = CID.parse(cid)
   return c.toV1().toString()
 }
+
+export function cidToGatewayUrl({cid, path, search = ''}, gatewayHost) {
+  // Parse and normalize CID
+  let nCid
+  try {
+    nCid = normalizeCid(cid)
+  } catch (err) {
+    throw new InvalidUrlError(`invalid CID: ${cid}: ${err.message}`)
+  }
+  return new URL(
+    `https://${nCid}.${gatewayHost}${path}${search}`
+  )
+}
+
+// https://link.xyz/https://example.org => https://example.org
+// https://link.xyz/https://gw.exz/ipfs/<cid>/<path> => https://<cid>.ipfs.nftstorage.link/path
+// https://link.xyz/ipfs://<cid>/<path> => ipfs://<cid>/<path> => https://<cid>.ipfs.nftstorage.link/path
+
+export function findPathUrl ({ pathname, search }, gatewayHost) {
+  const urlStr = pathname.substring(1)
+  if (urlStr.startsWith('ipfs://')) {
+    const cidPath = urlStr.substring('ipfs://'.length)
+    const [cid, ...rest] = cidPath.split('/')
+    return cidToGatewayUrl({ cid, path: '/' + rest.join('/'), search }, gatewayHost)
+  }
+  let url
+  try {
+    url = new URL(urlStr + search)
+  } catch (err) {
+    throw new InvalidUrlError(`invalid url ${urlStr}`)
+  }
+  if (url.pathname.startsWith('/ipfs/')) {
+    const [ _, cid, ...rest ] = url.pathname.substring(1).split('/')
+    return cidToGatewayUrl({ cid, path: '/' + rest.join('/'), search }, gatewayHost)
+  }
+  // otherwise pass thru.
+  return url
+}
